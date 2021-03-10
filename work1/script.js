eb588543c50a318affecc48ec1ee4c7638674a9e
ZarVarvar <vladimirzarifov@mail.ru>
2021-03-10 16:31:05 +0900

    final

---
+++ work1/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll(".button")[0].onclick = function() {
+	var i = document.querySelector(".element").value;
+	document.querySelectorAll(".text")[i-1].style.color = "red";
+};
+
+document.querySelectorAll(".button")[1].onclick = function() {
+	var i = document.querySelector(".element").value;
+	document.querySelectorAll(".text")[i-1].style.color = "green";;
+};
+
+document.querySelectorAll(".button")[2].onclick = function() {
+	var i = document.querySelector(".element").value;
+	document.querySelectorAll(".text")[i-1].style.color = "blue";
+};
